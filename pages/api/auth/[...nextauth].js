2c8591eba089a4ea098799c4c1cadf79040f94b3
thejasjanardhan <thejasj435@gmail.com>
2021-06-04 21:55:40 +0530

    bug fix

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,8 +5,9 @@ export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     Providers.Google({
-      clientId: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
+      clientId:
+        "806454835477-i7lgnosbej1qkc823v5m4elvcqlg4g8d.apps.googleusercontent.com",
+      clientSecret: "tZxG-oPgBprLfB9Ek1YQ0RZC",
     }),
     // ...add more providers here
   ],
